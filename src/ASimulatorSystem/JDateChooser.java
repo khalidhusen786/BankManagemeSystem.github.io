d04becb129fb3364c36630befa61c40ecb08a889
Khalid <khalidhusen000@gmail.com>
2022-01-20 22:26:11 +0545

    Initial Commit

---
+++ src/ASimulatorSystem/JDateChooser.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ASimulatorSystem;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author KHALID
+ */
+class JDateChooser {
+    public static void main(String[] args) {
+        JDateChooser jDateChooser1 = new JDateChooser(); //initialization
+jDateChooser1.setDateFormatString("dd/MM/yyyy");//change date format
+    }
+
+    private void setDateFormatString(String ddMMyyyy) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setForeground(Color color) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setBounds(int i, int i0, int i1, int i2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+    Object getDateEditor() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+//To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getText() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
+
+
